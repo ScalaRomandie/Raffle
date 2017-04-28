d074b306de03389d8f37fcbb42ebb834016b0b33
Antoine Comte <antoine@comte.cc>
2017-04-28 22:48:19 +0200

    Update for ScalaDays raffle

--- src/main/scala/org/scalaromandie/Raffle.scala
+++ src/main/scala/org/scalaromandie/Raffle.scala
@@ -10,35 +10,36 @@ import org.http4s.client.blaze.defaultClient
 import scala.util.Random
 
 /**
-  * Select the two winners of the Raffle using the meetup api
+  * Select the winner of the Raffle using the meetup api
   *
   */
 object Raffle extends App {
-  val client = defaultClient
 
   case class Member(name: String, role: Option[String])
-
   case class RSVP(member: Member, response: String)
 
-  val apiKey = System.getProperty("apikey")
+  Option(System.getProperty("apikey")) match {
+    case None => println("An api is required")
 
-  val rsvpsTask = {
-    val eventId = "234061340"
-    val target = (Uri.uri("https://api.meetup.com/Scala-Romandie/events/") / eventId / "rsvps")
-      .withQueryParam("key", apiKey)
-    client.expect(target)(jsonOf[List[RSVP]])
+    case Some(apiKey) =>
+      val client = defaultClient
+      val rsvpsTask = {
+        val eventId = "239576171"
+        val target = (Uri.uri("https://api.meetup.com/Scala-Romandie/events/") / eventId / "rsvps")
+          .withQueryParam("key", apiKey)
+        client.expect(target)(jsonOf[List[RSVP]])
+      }
+
+      val rsvps = rsvpsTask.run
+      val winners =
+        new Random(new SecureRandom()).shuffle(rsvps)
+          .filter(a => a.response == "yes" && a.member.role.isEmpty) /* role is empty for regular members, only present for Organisational roles */
+          .take(1)
+          .map(_.member.name)
+          .mkString("\n")
+
+      println(winners)
+
+      client.shutdownNow()
   }
-
-  val rsvps = rsvpsTask.run
-  val winners =
-     new Random(new SecureRandom()).shuffle(rsvps)
-      .filter(a => a.response == "yes" && a.member.role.isEmpty) /* role is empty for regular members, only present for Organisational roles */
-      .take(2)
-      .map(_.member.name)
-      .mkString("\n")
-
-  println(winners)
-
-  client.shutdownNow()
-
 }
