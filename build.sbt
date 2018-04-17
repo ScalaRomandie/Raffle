22a92b61f2ca23dbd1633e8064acadcfcde4d1ed
Antoine Comte <antoine@comte.cc>
2018-04-17 16:08:45 +0200

    Bump all the things

--- build.sbt
+++ build.sbt
@@ -2,17 +2,25 @@ name := "Raffle"
 
 version := "1.0"
 
-scalaVersion := "2.11.8"
+scalaVersion := "2.12.5"
 
 
-lazy val http4sVersion = "0.14.6"
+lazy val http4sVersion = "0.18.8"
 
 // Only necessary for SNAPSHOT releases
 
 libraryDependencies ++= Seq(
   "org.http4s" %% "http4s-dsl" % http4sVersion,
   "org.http4s" %% "http4s-blaze-client" % http4sVersion,
-  "org.http4s" %% "http4s-circe" %  http4sVersion,
-  "io.circe" %% "circe-generic" % "0.4.1",
-  "ch.qos.logback" % "logback-classic" % "1.1.7"
+  "org.http4s" %% "http4s-circe" % http4sVersion,
+  "io.circe" %% "circe-generic" % "0.9.3",
+  "ch.qos.logback" % "logback-classic" % "1.2.3"
+)
+
+scalacOptions ++= Seq(
+  "-Xlint",
+  "-unchecked",
+  "-deprecation",
+  "-feature",
+  "-Ypartial-unification"
 )
--- src/main/scala/org/scalaromandie/Raffle.scala
+++ src/main/scala/org/scalaromandie/Raffle.scala
@@ -2,54 +2,54 @@ package org.scalaromandie
 
 import java.security.SecureRandom
 
+import cats.data.Validated.{Invalid, Valid}
+import cats.data.ValidatedNel
+import cats.effect._
+import cats.implicits._
 import io.circe.generic.auto._
 import org.http4s.Uri
 import org.http4s.circe._
-import org.http4s.client.blaze.defaultClient
+import org.http4s.client.blaze._
 
 import scala.util.Random
-import scalaz._
-import Scalaz._
-
-import scalaz.concurrent.Task
-
 
 /**
   * Select the winner of the Raffle using the meetup api
   *
   */
 object Raffle extends App {
-
   case class Member(name: String, role: Option[String])
-
   case class RSVP(member: Member, response: String)
+  case class Params(apiKey: String, eventId: String)
+  val random = new Random(new SecureRandom())
 
+  readParams() match {
+    case Valid(params) =>
+      val url =
+        (Uri.uri("https://api.meetup.com/Scala-Romandie/events/") / params.eventId / "rsvps")
+          .withQueryParam("key", params.apiKey)
 
-  (readParam("apikey") |@| readParam("eventid")) (_ -> _)
-  match {
+      Http1Client[IO]()
+        .flatMap(_.expect(url)(jsonOf[IO, List[RSVP]]))
+        .map { rsvps =>
+          val candidates =
+            random
+              .shuffle(rsvps)
+              .filter(a => a.response == "yes" && a.member.role.isEmpty) /* role is empty for regular members, only present for Organisational roles */
+              .map(_.member.name)
 
-    case Failure(f) => f.foreach(println(_))
+          candidates.take(1).map("* " + _) ::: candidates.drop(1)
+        }
+        .map(_.foreach(println(_)))
+        .unsafeRunSync
 
-    case Success(params) =>
-      val (apiKey, eventId) = params
-      val client = defaultClient
-      val rsvpsTask = {
-        val target = (Uri.uri("https://api.meetup.com/Scala-Romandie/events/") / eventId / "rsvps")
-          .withQueryParam("key", apiKey)
-        client.expect(target)(jsonOf[List[RSVP]])
-      }.map { rsvps =>
-          new Random(new SecureRandom())
-            .shuffle(rsvps)
-            .filter(a => a.response == "yes" && a.member.role.isEmpty) /* role is empty for regular members, only present for Organisational roles */
-            .take(1)
-            .map(_.member.name)
-            .foreach(println(_))
-      }
-        .onFinish(_ => Task(client.shutdownNow()))
-
-      rsvpsTask.run
+    case Invalid(f) => f.toList.foreach(println(_))
   }
 
-  def readParam(paramName: String): ValidationNel[String, String] =
-    Option(System.getProperty(paramName)).toSuccessNel(s"Missing parameter $paramName")
+  def readParams(): ValidatedNel[String, Params] =
+    (readParam("apikey"), readParam("eventid")) mapN (Params.apply)
+
+  def readParam(paramName: String): ValidatedNel[String, String] =
+    Option(System.getProperty(paramName))
+      .toValidNel(s"Missing parameter $paramName")
 }
